9b292a859e42b47ee61a49f8b9c822889b66d9f8
Jakub Klimek <etianoo@gmail.com>
2020-09-23 18:32:27 +0200

    Update player.js

--- src/player/player.js
+++ src/player/player.js
@@ -190,9 +190,7 @@ export const player = Object.assign({}, {
                 song.file = makeid(10)+".mp3";
 
             player.YD.download(ytid, song.file);
-        }).catch(e=>{
-            console.log(e);
-        });
+        })
     },
     downloadSongs(ytids){
         if(cfg.demo){
